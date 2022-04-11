760998fe95e55b8dc93502806e60dc99fede239d
wgetgoose <louiscarsonferreira@gmail.com>
2022-04-11 13:54:29 -0700

    verbose comments, switch statement cleanup

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,11 @@
 /*
-    ***** TO-DOs ******
+    ***** TO-DOs *****
+    1. Check whether 'operated' is necessary
 */
+
+
+// When I say 'chain calculation' I mean when the user enters
+// multiple operators in a row, i.e: 10 - 2 + 3
 
 let array = [] // store numbers
 let operator;
@@ -9,6 +14,7 @@ let operated = false; // boolean used in event listener for numbers input
 const screen = document.getElementById("screen");
 const currentInput = document.getElementById("current-input")
 
+// change strings like 'add' into symbols
 function refreshScreen() {
     string = `${array[0]} ${operator} ${array[1]}`
     string = string.replace('add', '+');
@@ -18,6 +24,7 @@ function refreshScreen() {
     currentInput.textContent = string.replace(/undefined/g, '')
 }
 
+// moves computed value into array[0] for chain calculation
 const reset = () => {
     array[0] = calculate(); 
     screen.textContent = array[0];
@@ -27,36 +34,38 @@ const reset = () => {
 }
 
 const calculate = () => {
-    switch(array[0]) {
-        case (undefined && (array[1] == undefined)):
+    // filters edge cases, like 10 div 0 and user pressing equals button
+    // This did not work when passing array[0] into the switch expression
+    switch(true) { 
+        case (array[0] == undefined):
             return 'ERR';
-        case (!undefined && (array[1] == undefined)):
+        case (array[0] != undefined) && ( (array[1] == '0') && (operator == 'divide') ):
+            return 'ERR';
+        case (array[0] != undefined) && (array[1] == undefined):
             return array[0];
-        case ('0' && (operator == 'divide')):
-            return 'ERR';
+    };
+    operated = true;
+    array[0] = Number(array[0])
+    array[1] = Number(array[1])
+    switch(operator) {
+        case 'add':
+            return array[0] + array[1];
+        case 'subtract': 
+            return array[0] - array[1];
+        case 'multiply':
+            return array[0] * array[1];
+        case 'divide':
+            return array[0] / array[1];
+        default:
+            return 'OOPS';
     }
-    return array.reduce( (previous, current) => {
-        operated = true;
-        previous = Number(previous)
-        current = Number(current)
-        switch (operator) {
-            case 'add':
-                return previous + current;
-            case 'subtract': 
-                return previous - current;
-            case 'multiply':
-                return previous * current;
-            case 'divide':
-                return previous / current;
-            default:
-                return 'OOPS';
-        }
-    })
 }
+
 
 const numbers = Array.from(document.getElementsByClassName("num"));
 numbers.forEach(element => {
     element.addEventListener("click", e => {
+        // Multi-digit number entry 
         switch(true) {
             case (operated == true) && (operator == undefined):
                 screen.textContent = screen.textContent + (e.target.id);
